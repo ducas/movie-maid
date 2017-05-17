35baf4f75f7f6de481d9d5367a7a2f436afb0c35
Ducas Francis <ducas@ducasfrancis.com>
2017-05-17 16:05:59 +1000

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'Move TV',
+      script    : 'app.js',
+      args      : ['-p', '-s', '/mnt/library/Downloads/complete/TV', '-d', '/mnt/library/TV']
+    },
+
+    {
+      name      : 'Move Movies',
+      script    : 'app.js',
+      args      : ['-s', '/mnt/library/Downloads/complete/Movies', '-d', '/mnt/library/Movies']
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'pi',
+      host : '10.1.1.15',
+      ref  : 'origin/deploy',
+      repo : 'git@github.com:ducas/movie-maid.git',
+      path : '/home/pi/movie-maid-deploy',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
